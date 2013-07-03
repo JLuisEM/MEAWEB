cc28eaf90b8c18b8dce5284d7b61be23836f18d1
Jose Luis Estrada Molina <joseluisem@live.com>
2013-07-02 18:15:09 -0600

    MOD2

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,18 +1,37 @@
 $(document).ready(function() {
-	$("#slideshow").css("overflow", "hidden");
-	
-	$("ul#slides").cycle({
-		fx: 'fade',
-		pause: 1,
-		prev: '#prev',
-		next: '#next'
-	});
-	
-	$("#slideshow").hover(function() {
-    	$("ul#navslides").fadeIn();
-  	},
-  		function() {
-    	$("ul#navslides").fadeOut();
-  	});
-	
+
+function slideSwitch() {
+    var $active = $('#slideshow DIV.active');
+
+    if ( $active.length == 0 ) $active = $('#slideshow DIV:last');
+
+    // use this to pull the divs in the order they appear in the markup
+    var $next =  $active.next().length ? $active.next()
+        : $('#slideshow DIV:first');
+
+    // uncomment below to pull the divs randomly
+    // var $sibs  = $active.siblings();
+    // var rndNum = Math.floor(Math.random() * $sibs.length );
+    // var $next  = $( $sibs[ rndNum ] );
+
+
+    $active.addClass('last-active');
+
+    $next.css({opacity: 0.0})
+        .addClass('active')
+        .animate({opacity: 1.0}, 1000, function() {
+            $active.removeClass('active last-active');
+        });
+}
+
+$(function() {
+    setInterval( "slideSwitch()", 5000 );
 });
+
+
+});
+
+
+
+
+
