511e41f3284171a0efd8d39c4d20f4070a194bb1
Jose Luis Estrada Molina <joseluisem@live.com>
2013-07-09 09:40:09 -0600

    MOD4
    Ultima Mod Personal

---
+++ config-formulario.php
@@ -0,0 +1,7 @@
+<?php
+$privatekey = "6Lc0MdUSAAAAAN7NUSRo_O1YJUGHybcYLKb63e5q";  // La llave privada que genera en https://www.google.com/recaptcha/admin/create
+$publickey = "6Lc0MdUSAAAAAFDjKKjgwq6RsmgJzbtQPXbqWm6s";  // La llave publica que genera en https://www.google.com/recaptcha/admin/create
+$enviar_a = "tudireccion@Email.com"; // La dirección Email donde recibirá los mensajes enviados a través del formulario
+$asunto = "Formulario de Contacto"; // El asunto del mensaje
+$pagina_confirmacion = "gracias.html"; // La página de confirmación de recepción ( puede dejar cómo está )
+?>
